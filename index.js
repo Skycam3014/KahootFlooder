d581ebd8a75ea83899a8d36e23f3231017f16140
samleews <72475146+samleews@users.noreply.github.com>
2021-01-08 14:22:54 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {Client} = require('discord.js');
+const {Client, MessageEmbed} = require('discord.js');
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const sq = require('./flood');
@@ -7,5 +7,16 @@ const kahoot = require("kahoot-spam")
 client.on("ready", () => {
   console.log("I am ready!");
 });
+
+  client.on("message", (message) => {
+    if (message.content.startsWith("!help")) {
+
+  const embed = new MessageEmbed();
+  embed.setTitle("KahootFlood Guide");
+  embed.setColor("#00ffff");
+  embed.setDescription("!flood (game pin goes here) - Flood any Kahoot game with bots");
+  message.channel.send(embed);
+    }
+  });
 
 client.login(process.env.BOT_TOKEN);

fe2634a84ce13d7bd7a7a8ea5709f93ee3c17f52
samleews <72475146+samleews@users.noreply.github.com>
2021-01-08 14:19:39 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const {Client} = require('discord.js');
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const sq = require('./flood');
-const config = require('./config.json');
 const kahoot = require("kahoot-spam")
 
 client.on("ready", () => {

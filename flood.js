0f303f468e9a08eb49db8bd22ac848ee1e0ff045
samleews <72475146+samleews@users.noreply.github.com>
2021-01-11 14:06:39 -0800

    Update flood.js

--- flood.js
+++ flood.js
@@ -12,10 +12,13 @@ client.on("ready", () => {
 
 client.on("message", (message) => {
   if (message.content.startsWith("!flood")) {
+    if (talkedRecently.has(message.author.id)) {
+      message.channel.send("Wait 1 minute before getting typing this again. - " + message.author);
+    } else {
     for (i = 0; i < 5; i++) {
       let api = KahootSpam
       var pin = (message.content.replace('!flood ', ''));
-      api.spamWithAnswers(pin, "Fuck", 40, 1)
+      api.spamWithAnswers(pin, "Fuck", 40, 100)
     }
         console.log(message.author);
         const embed = new MessageEmbed();
@@ -24,7 +27,14 @@ client.on("message", (message) => {
         embed.setDescription("Flooding Kahoot game " + (message.content.replace('!flood ', '')));
         message.channel.send(embed);
         console.clear()
+        talkedRecently.add(message.author.id);
+        setTimeout(() => {
+        // Removes the user from the set after a minute
+        talkedRecently.delete(message.author);
+        }, 60000);
+        }
     }
+
   });
 
 client.login(process.env.BOT_TOKEN);
